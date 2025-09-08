7970710c92765bd6173bc73f0ea236d2b59a7048
Saba-new <k.sabarish2005@gmail.com>
2025-09-08 20:58:37 +0530

    Time: 14 ms (11.59%), Space: 45 MB (20.61%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValidSudoku(char[][] b) {
+        Set s=new HashSet<>();
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                char c=b[i][j];
+                if(c!='.'){
+                    if(!s.add(c+"in row"+i)||
+                        !s.add(c+"in column"+j)||
+                        !s.add(c+"b"+i/3+"."+j/3)){
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+    }
+
