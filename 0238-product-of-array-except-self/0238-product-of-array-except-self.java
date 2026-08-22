6166a6a057cfda6fac1627039791c67ec3dc618b
SABARISH K <k.sabarish2005@gmail.com>
2026-08-22 11:44:00 +0530

    Time: 2 ms (93.67%), Space: 71.9 MB (60.12%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,36 +1,18 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        int[] ans = new int[n];
-        left[0]=1;
-        int i;
-        for(i=1;i<=n-1;i++)  //left = [ 1, 1 , 2 , 6 ]
-        {
-            left[i]=left[i-1]*nums[i-1];
-            //left[1] = left[2] * nums[0]
-                      //  2 * 3 = 6
+        int n=nums.length;
+        int[]arr=new int[n];
+        int product=1;
+        for(int i=0;i<n;i++){
+            arr[i]=product;
+            product=product*nums[i];
+            
         }
-         
-        right[n-1] = 1;
-
-        for(i=n-2;i>=0;i--) //right = []
-        {
-            right[i]=right[i+1] * nums[i+1];
-            //right[2] = right[3] * nums[3] = 1 * 4 = 4
-            //right[1] = right[2] * nums[2] = 4 * 3 = 12 
-            //right[0] = right[1] * nums[1] = 12 * 2 = 24
+        int suffix=1;
+        for(int i=n-1;i>=0;i--){
+            arr[i]*=suffix;
+            suffix=suffix*nums[i];
         }
-
-        for(i=0;i<=n-1;i++)
-        {
-            ans[i]=left[i] * right[i];
-                    //1 * 24 
-                    //1 * 12 
-                    //2 * 4
-                    //6 * 1   
-        }
-        return ans;
+        return arr;
     }
 }
